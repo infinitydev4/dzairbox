41f5aceb6666e1c1d1c5ff9aa5d868251a239e9f
homdev <homdev44@gmail.com>
2025-06-23 19:12:14 +0200

    migration add field to user

---
+++ prisma/migrations/20250623171136_add_field_to_user_model/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Business" ADD COLUMN     "heroImage" TEXT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "address" TEXT,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "phone" TEXT;
